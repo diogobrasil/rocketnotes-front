3bf32c1e2d5e4f161917a02e43bd27b04d417582
diogo <diogobrasildasilva@gmail.com>
2024-06-07 16:00:05 -0300

    Utilização do localStorage e do useEffect para garantir que o usuário continue logado mesmo recarregando a página ou fechando o navegador.

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
 
@@ -13,7 +13,11 @@ function AuthProvider ( { children}) {
     try {
       const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
-      
+
+      localStorage.setItem("@rocketnotes : token", token);
+      localStorage.setItem("@rocketnotes : user", JSON.stringify(user));
+      //O localstorage salva as informações de autenticação no navegador do usuário.
+
       api.defaults.headers.authorization = `Bearer ${token}`;
       setData({ user, token });
     }
@@ -28,6 +32,18 @@ function AuthProvider ( { children}) {
     }
 
   }
+
+  useEffect ( () => {
+
+    const token = localStorage.getItem("@rocketnotes : token");
+    const user = JSON.parse(localStorage.getItem("@rocketnotes : user"));
+
+    if ( token && user ) {
+
+      api.defaults.headers.authorization = `Bearer ${token}`;
+      setData({ user, token });
+    }
+  }, []);//Esse useEffect disapra sempre que a página é recarregada buscando os valores no localstorage e armazenando dentro do state data.
 
   return (
     <AuthContext.Provider value={{ signIn, user : data.user }}>
