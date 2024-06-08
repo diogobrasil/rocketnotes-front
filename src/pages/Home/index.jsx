a6fcf97a0d445c8833024c891cdf898023ccf22c
diogo <diogobrasildasilva@gmail.com>
2024-06-07 22:07:02 -0300

    Implementação dos filtros e da barra de pesquisa para exibição de notas na home do usuário.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import { FiPlus, FiSearch } from 'react-icons/fi';
 
-import { Container, Brand, Menu, Search, Content, NewNote} from './style';
+import { Container, Brand, Menu, Search, Content, NewNote } from './style';
 
 import { Header } from '../../components/header';
 import { Section } from '../../components/section';
@@ -8,7 +8,59 @@ import { Input } from '../../components/input';
 import { Note } from '../../components/note';
 import { ButtonText } from '../../components/buttonText';
 
-export function Home () {
+import { useState, useEffect } from 'react';
+import { api } from '../../services/api';
+
+export function Home() {
+
+  const [ notes, setNotes ] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [tagsSelected, setTagsSelected] = useState([]);
+  const [ search, setSearch ] = useState("");
+
+
+
+  function handleTagsSelected(tagName) {
+    /* Função que altera a cor dos buttons que servirão como filtros para as tags.
+    * Se a tag já estiver selecionada, ela será removida.
+    * Se não estiver, ela será adicionada.
+    */
+    if ( tagName === "all") {
+      return setTagsSelected( [] );
+    }
+  
+    if (tagsSelected.includes(tagName)) {
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName);
+      setTagsSelected(filteredTags);
+    }
+    else {
+      setTagsSelected(prevState => [...prevState, tagName]);
+    }
+
+  }
+  //useEffect que altera o nome dos filtros.
+  useEffect(() => {
+    //Função assíncrona que busca as tags no banco de dados.
+    async function fetchTags() {
+      const response = await api.get("/tags");
+      setTags(response.data);
+    }
+
+    fetchTags();
+
+  }, []);
+  //useEffect que busca as notas de acordo com o filtro selecionado e/ou com o titulo pesquisado.
+  useEffect (() => {
+
+    async function fetchNotes() {
+      const response = await api.get(`/notes?title=${search}&tags=${tagsSelected}`);
+      setNotes(response.data);
+    }
+
+    fetchNotes();
+
+  }, [search, tagsSelected]);
+
   return (
     <Container>
       <Brand>
@@ -17,46 +69,56 @@ export function Home () {
         </h1>
       </Brand>
 
-      <Header/>
+      <Header />
 
       <Menu>
 
         <li>
-          <ButtonText title="Todos" />
+          <ButtonText
+            title="Todos"
+            onClick={() => handleTagsSelected("all")}
+            isActive={tagsSelected.length === 0}
+          />
         </li>
 
-        <li>
-          <ButtonText title="React"/>
-        </li>
-
-        <li>
-          <ButtonText title="NodeJS"/>
-        </li>
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)}>
+              <ButtonText
+                title={tag.name}
+                onClick={() => handleTagsSelected(tag.name)}
+                isActive={tagsSelected.includes(tag.name)}
+              />
+            </li>
+          ))
+        }
 
       </Menu>
 
       <Search>
-        <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
+        <Input
+          placeholder="Pesquisar pelo título"
+          icon={FiSearch}
+          onChange={event => setSearch(event.target.value)}
+        />
       </Search>
 
       <Content>
         <Section title="Minhas Notas">
-
-          <Note data={
-            {
-              title : 'React',
-              tags : [
-                {id : '1', name : 'react'},
-                {id : '2', name : 'rocketseat'}
-              ]
-            }
-          }/>
-
+          {
+            notes.map( note => (
+                <Note 
+                  key={String(note.id)}
+                  data={note}
+                />
+              )
+            )
+          }
         </Section>
       </Content>
 
       <NewNote to="/new">
-        <FiPlus/>
+        <FiPlus />
         Criar Nota
       </NewNote>
 
